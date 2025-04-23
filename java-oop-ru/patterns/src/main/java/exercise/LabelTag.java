d8925fab1b8b0595b47cc4340f57ab2ab665b0aa
DSunShine371 <pestic077@gmail.com>
2025-04-23 22:30:58 +0300

    Result

--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -1,5 +1,18 @@
 package exercise;
 
 // BEGIN
+public class LabelTag implements TagInterface {
+    String label;
+    TagInterface tagInterface;
 
+    public LabelTag(String label, TagInterface inputTag) {
+        this.label = label;
+        this.tagInterface = inputTag;
+    }
+
+    @Override
+    public String render() {
+        return String.format("<label>%s%s</label>", label, tagInterface.render());
+    }
+}
 // END
