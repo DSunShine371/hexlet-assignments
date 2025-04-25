5d6dc27774c81c31f25258344d0ae8b51e4dbb93
DSunShine371 <pestic077@gmail.com>
2025-04-25 13:12:04 +0300

    Result

--- java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
+++ java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
@@ -1,5 +1,12 @@
 package exercise;
 
 // BEGIN
-
+public class NegativeRadiusException extends Exception {
+    public NegativeRadiusException() {
+        super();
+    }
+    public NegativeRadiusException(String message) {
+        super(message);
+    }
+}
 // END
