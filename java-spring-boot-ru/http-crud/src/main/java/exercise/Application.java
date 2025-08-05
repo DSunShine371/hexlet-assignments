fd9ef6fde20fc23c2b36a6c31a207e7a1e819f18
DSunShine371 <pestic077@gmail.com>
2025-08-05 16:14:02 +0300

    result

--- java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -27,6 +27,41 @@ public class Application {
     }
 
     // BEGIN
-    
+    @GetMapping("/posts")
+    public List<Post> index(@RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream().limit(limit).toList();
+    }
+
+    @PostMapping("/posts") // Создание страницы
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var maybePage = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (maybePage.isPresent()) {
+            Post post = maybePage.get();
+            post.setId(data.getId());
+            post.setTitle(data.getTitle());
+            post.setBody(data.getBody());
+        }
+        return data;
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
     // END
 }
