b955ac8c62db30b4251f60fdf6b177aa5d633476
DSunShine371 <pestic077@gmail.com>
2025-08-05 15:21:35 +0300

    result

--- java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
@@ -9,7 +9,13 @@ public class Application {
         var address = new Address("London", 12345678);
 
         // BEGIN
-        
+        for (Method method : Address.class.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Inspect.class)) {
+                String methodName = method.getName();
+                String returningType = method.getReturnType().getSimpleName();
+                System.out.printf("Method %s returns a value of type %s%n", methodName, returningType);
+            }
+        }
         // END
     }
 }
