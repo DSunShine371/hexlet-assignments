2b5f831ed612b18db051393e24f622c2b85a9821
DSunShine371 <pestic077@gmail.com>
2025-08-06 17:45:19 +0300

    result

--- java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,7 +1,10 @@
 package exercise.controller.users;
 
 import java.util.List;
+
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,5 +17,31 @@ import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    @Setter
+    private static  List<Post> posts = Data.getPosts();
 
+    @GetMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Post> index(@PathVariable Integer id) {
+        return posts.stream()
+                .filter(p -> p.getUserId() == id)
+                .toList();
+    }
+
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity<Post> create(@RequestBody Post post, @PathVariable Integer id) {
+        Post responsePost = new Post();
+        responsePost.setUserId(id);
+        responsePost.setSlug(post.getSlug());
+        responsePost.setTitle(post.getTitle());
+        responsePost.setBody(post.getBody());
+
+        posts.add(responsePost);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responsePost);
+    }
+}
 // END
