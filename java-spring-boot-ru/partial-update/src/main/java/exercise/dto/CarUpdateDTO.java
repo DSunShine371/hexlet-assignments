f53417e13ba2859b7bb6658c74b00592454b65a7
DSunShine371 <pestic077@gmail.com>
2025-08-14 15:47:44 +0300

    result

--- java-spring-boot-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
+++ java-spring-boot-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -7,5 +7,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
-
+@Setter
+@Getter
+public class CarUpdateDTO {
+    private JsonNullable<String> model;
+    private JsonNullable<String> manufacturer;
+    private JsonNullable<Integer> enginePower;
+}
 // END
